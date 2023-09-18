a894af9c7c950a8e604f5751e81d69010e080334
devgiljong <calf-love@naver.com>
2023-09-18 18:39:42 +0900

    :sparkles: Scrollbar practice
    add

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,51 @@ import Component from './_components/Component';
 
 export default function Home() {
   return (
-    <main>
+    <main
+      className={`${css({
+        w: 500,
+        h: 400,
+        display: 'flex',
+        overflow: 'scroll',
+      })} content`}
+    >
       <div className={circle({ size: 24, overflow: 'hidden' })}></div>
-      <div className={hstack({ gap: 4 })}></div>
-      <Component />
+      <div
+        className={hstack({ gap: 4 })}
+        style={{
+          scrollbarColor: 'red',
+        }}
+      ></div>
+      <div
+        className={`${css({
+          w: '40px',
+          h: '20px',
+        })}`}
+      >
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+        dsadsads dsadsads dsadsads dsadsads
+      </div>
     </main>
   );
 }
