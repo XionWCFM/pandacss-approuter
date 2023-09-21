9e4ead5eb989cae79ca9c4a52a3440f549a9d3cd
devgiljong <calf-love@naver.com>
2023-09-21 22:05:20 +0900

    :recycle: [refactor]

--- app/page.tsx
+++ app/page.tsx
@@ -4,51 +4,64 @@ import Component from './_components/Component';
 
 export default function Home() {
   return (
-    <main
-      className={`${css({
-        w: 500,
-        h: 400,
-        display: 'flex',
-        overflow: 'scroll',
-      })} content`}
-    >
-      <div className={circle({ size: 24, overflow: 'hidden' })}></div>
-      <div
-        className={hstack({ gap: 4 })}
-        style={{
-          scrollbarColor: 'red',
-        }}
-      ></div>
-      <div
+    <div className="">
+      <main
         className={`${css({
-          w: '40px',
-          h: '20px',
-        })}`}
+          w: 500,
+          h: 400,
+          display: 'flex',
+          overflow: 'scroll',
+        })} content`}
       >
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
-        dsadsads dsadsads dsadsads dsadsads
+        <div className={circle({ size: 24, overflow: 'hidden' })}></div>
+        <div
+          className={hstack({ gap: 4 })}
+          style={{
+            scrollbarColor: 'red',
+          }}
+        ></div>
+        <div
+          className={`${css({
+            w: '40px',
+            h: '20px',
+          })}`}
+        >
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads dsadsads
+          dsadsads dsadsads dsadsads dsadsads dsadsads
+        </div>
+      </main>
+      <div
+        className={css({
+          height: 2000,
+          w: 1200,
+        })}
+      >
+        dsa
       </div>
-    </main>
+    </div>
   );
 }
